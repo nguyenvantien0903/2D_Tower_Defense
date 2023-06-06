78a077970682529a779c37024c2b31e4b80a225d
NVTFT <65675853+nguyenvantien0903@users.noreply.github.com>
2023-06-07 06:17:04 +0700

    aa

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Attributes")]
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Transform target;
+    private int pathIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = LevelManager.instance.path[pathIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // khoang cach giua ke dich va noi ke dich can den
+        if(Vector2.Distance(target.position, transform.position) <=0.1f)
+        {
+            pathIndex++;
+            //Neu da di den cuoi
+            if(pathIndex==LevelManager.instance.path.Length)
+            {
+                Destroy(gameObject);
+                return;
+            }
+        }
+    }
+}
