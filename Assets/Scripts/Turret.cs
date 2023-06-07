811ae8c1aa025356f539082641409f181eabbd0a
NVTFT <65675853+nguyenvantien0903@users.noreply.github.com>
2023-06-07 12:28:36 +0700

    sung ngam theo enemy

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Transform turretRotationPoint;
+    [SerializeField] private LayerMask enemyMask;
+
+    [Header("Attributes")]
+    [SerializeField] private float targetingRange = 5.0f;
+    [SerializeField] private float rotationSpeed = 5.0f;
+
+    private Transform target;
+    private void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.cyan;
+        Handles.DrawWireDisc(transform.position,transform.forward, targetingRange);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target == null)
+        {
+            FindTarget();
+            return;
+        }
+        RotateTowardsTarget();
+        if (!IsTargetInRange())
+        {
+            target = null;
+        }
+    }
+
+    private bool IsTargetInRange()
+    {
+        return Vector2.Distance(target.position, transform.position) <= targetingRange;
+    }
+
+    private void RotateTowardsTarget()
+    {
+        float angle = Mathf.Atan2(target.position.y-transform.position.y,target.position.x-transform.position.x)*Mathf.Rad2Deg - 90f;
+        Quaternion targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+        turretRotationPoint.rotation = Quaternion.RotateTowards(turretRotationPoint.rotation,targetRotation,rotationSpeed*Time.deltaTime);
+    }
+
+    private void FindTarget()
+    {
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, targetingRange, (Vector2)transform.position, 0f, enemyMask);
+        if(hits.Length > 0 )
+        {
+            target = hits[0].transform;
+        }
+    }
+}
